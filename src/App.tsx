bcdbc3c1f180ee2fff86e07221563de37a2b6742
sasank prajapati <sasank45prajapati@gmail.com>
2025-09-18 10:25:08 +0000

    chore: add initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import AppRoutes from './routes/AppRoutes'
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <AppRoutes />
+    </div>
+  )
+}
+
+export default App
